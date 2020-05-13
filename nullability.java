3181ab39d8346ae9980d12f94850e9f881a23232
sidchiku9 <sidchiku9@gmail.com>
2020-05-13 13:37:43 +0530

    nullability

---
+++ nullability.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class nullability{
+    public static void main(String args[]){
+        Integer canbenull;
+        int cantbenull;
+
+        canbenull = null;
+        cantbenull = 0;
+
+        //Integers are nullable but int are not.
+
+        System.out.print("Successful!");
+    }
+}
