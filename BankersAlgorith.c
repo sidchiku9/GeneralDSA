60c5aaab75ef990bbae80516a036f3cad6478ea1
sidchiku9 <sidchiku9@gmail.com>
2020-08-23 18:41:01 +0530

    defne process and input

---
+++ BankersAlgorith.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    int process, resources ;
+
+    printf("How many processes do you have ? \n");
+    scanf("%d",&process);
+
+    printf("How many resources do you have ? \n");
+    scanF("%d",&resources);
+
+    
+}
