b036beedbab041b8aa5b84b9ea057fb3f18c983e
sidchiku9 <sidchiku9@gmail.com>
2020-08-23 18:46:25 +0530

    define arrays

--- BankersAlgorith.c
+++ BankersAlgorith.c
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 
 int main(){
-    int process, resources ;
+    int process, resources, i ;
 
     printf("How many processes do you have ? \n");
     scanf("%d",&process);
@@ -10,5 +10,15 @@ int main(){
     printf("How many resources do you have ? \n");
     scanF("%d",&resources);
 
-    
+    int allocation[process][resources], maxNeed[process][resources], 
+    availableRes[process][resources], remNeed[process][resources], totalResources[resources];
+
+    printf("Enter the total number of resources : \n");
+
+    for(i = 0 ; i < resources ; i++){
+        printf("Resource %d : ",i);
+        scanf("%d",&totalResources[resources]);
+    }
+
+
 }
