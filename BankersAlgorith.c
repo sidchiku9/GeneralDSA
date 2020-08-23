11d4112d417d4db3ffbbde6b95813a534b61efc7
sidchiku9 <sidchiku9@gmail.com>
2020-08-24 01:45:46 +0530

    target

--- BankersAlgorith.c
+++ BankersAlgorith.c
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 
 int main(){
-    int process, resources, i, j ;
+    int process, resources, i, j, z ;
 
     printf("How many processes do you have ? \n");
     scanf("%d",&process);
@@ -11,8 +11,7 @@ int main(){
     scanf("%d",&resources);
 
     int allocation[process][resources], maxNeed[process][resources], 
-    availableRes[process][resources], remNeed[process][resources], totalResources[resources],
-    allocatedSum[resources];
+    availableRes[resources], remNeed[process][resources], totalResources[resources];
 
     printf("Enter the total number of resources : \n");
 
@@ -30,28 +29,24 @@ int main(){
         }
     }
 
-    printf("Enter the max need of resources : \n");
+   printf("Enter the max need of resources : \n");
 
     for(i = 0 ; i < process ; i++){
         for( j = 0 ; j < resources ; j++){
             printf("Process%d Resource%d ",i,j);
             scanf("%d",&maxNeed[i][j]);
         }
-    }
+    } 
 
-    //finding out total allocated resources in order to calculate available resources
-    for(i = 0 ; i < resources ; i++){
-        for(j = 0 ; j < process ; j++){
-            allocatedSum[i] += allocation[i][j];
-        }
-    }
 
-    //available resources
+   printf("Enter the available number of resources : \n");
+
     for(i = 0 ; i < 1 ; i++){
-        for(j = 0 ; j < resources ; j++){
-            availableRes[i][j] = totalResources[j] - allocatedSum[j];
+        for( j = 0 ; j < resources ; j++){
+            printf("Resource%d : \n",j);
+            scanf("%d",&availableRes[j]);
         }
-    }
+    } 
 
     //remaining need
     for(i = 0 ; i < process ; i++){
@@ -63,16 +58,17 @@ int main(){
     for(i = 0 ; i < process ; i++){
         for(j = 0 ; j < resources ; j++ ){
 
-            if(availableRes[0][j] >= remNeed[i][j]){
+            if(availableRes[j] >= remNeed[i][j]){
                 printf("\n This works fine : %d",i);
-                //coudl have used pop here to remove satisfied element
+                //could have used pop here to remove satisfied element
             }
             else{
-                return;
+                printf("Deadlock \n");
+                continue;
             }
         }
     }
 
     return 0;
-    
+
 }
