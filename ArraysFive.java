d5d908cada2d3d4d95c8236edd9943c615f6f4f1
sidchiku9 <sidchiku9@gmail.com>
2020-08-26 15:12:43 +0530

    COMMITS

--- ArraysFive.java
+++ ArraysFive.java
@@ -1,3 +1,36 @@
+import java.util.Random;
+
 public class ArraysFive {
-    
+    int sumArr(int arr[]){
+        int sum = 0 ;
+
+        for(int i = 0 ; i < 10 ; i++){
+            sum += arr[i];
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args){
+        int arr[] = new int[10];
+        int sum = 0;
+
+        ArraysFive arraysfive = new ArraysFive();
+
+        Random random = new Random();
+
+        for(int i = 0 ; i < 10 ; i++){
+            arr[i] = random.nextInt(1000);
+        }
+
+        System.out.println("Printing the array : ");
+
+        for(int i = 0 ; i < 10 ; i++){
+            System.out.println(arr[i]);
+        }
+
+        sum = arraysfive.sumArr(arr);
+
+        System.out.println("The sum of the array is : " + sum);
+    }
 }
--- ArraysFour.java
+++ ArraysFour.java
@@ -1,3 +1,76 @@
+import java.util.*;
+
 public class ArraysFour {
-    
+    Scanner scanner = new Scanner(System.in);
+
+    void getArray(int arr[], int elements) {
+        System.out.println("Please start enetering the array elements");
+
+        for (int i = 0; i < elements; i++) {
+            arr[i] = scanner.nextInt();
+        }
+    }
+
+    void printArray(int arr[], int elements) {
+        System.out.println("Printing the array");
+
+        for (int i = 0; i < elements; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+    int duplicate(int arr[], int element) {
+
+        getArray(arr, element);
+
+        if (element == 0 || element == 1) {
+            return element;
+        }
+
+        int j = 0;
+
+        for (int i = 0; i < element - 1; i++) {
+            if (arr[i] != arr[i + 1]) {
+                arr[j++] = arr[i];
+            }
+        }
+
+        arr[j++] = arr[element - 1];
+        return j;
+    }
+
+    public static void main(String[] args) {
+
+        int length = 0, element = 0, choice = 0;
+
+        Scanner scanner = new Scanner(System.in);
+
+        ArraysFour af = new ArraysFour();
+
+        do {
+            System.out.println("1 : Remove duplicates form the array");
+            System.out.println("2 : Exit the program");
+
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.printf("How many elements do you want in your array ? ");
+                    element = scanner.nextInt();
+                    int arr[] = new int[element];
+                    length = af.duplicate(arr, element);
+                    System.out.println("The size of the new array after removing duplicates : " + length);
+                    break;
+
+                case 2:
+                    System.exit(1);
+
+                default:
+                    System.out.println("Invalid input. Please try again.");
+                    break;
+            }
+        } while (choice != 2);
+
+        scanner.close();
+    }
 }
