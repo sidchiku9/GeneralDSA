5af8b0588cfcafddf2fcc74c715d7bc6c25fd5b4
sidchiku9 <sidchiku9@gmail.com>
2020-09-02 09:45:47 +0530

    insertatbeginning

--- linkedlistOne.cpp
+++ linkedlistOne.cpp
@@ -1,4 +1,5 @@
 #include<stdlib.h>
+#include<iostream>
 
 using namespace std;
 
@@ -14,17 +15,29 @@ struct Node{
 
 //insert at the beginning of the linked list
 
-Node *insertAtBeginning(Node *head, int x){
-    Node *temp = new Node(x);
-    temp -> next = NULL;
+Node *insertAtBeginning(Node *head, int key){
+    Node *temp = new Node(key);
+    temp -> next = head;
     return temp;
+}
+
+void printList(Node *head){
+    Node *current = head;
+
+    while(current != NULL){
+        cout << current -> key << " -> ";
+        current = current -> next;
+    }
 }
 
 int main(){
     Node *head = NULL;
+
     head = insertAtBeginning(head, 20);
     head = insertAtBeginning(head, 5);
     head = insertAtBeginning(head, 8);
+
+    printList(head);
 
     return 0;
 }
