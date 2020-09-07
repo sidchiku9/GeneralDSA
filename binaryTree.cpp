a4014ff03fa3568949532885e9a3d1c44a2a0ccd
sidchiku9 <sidchiku9@gmail.com>
2020-09-07 15:25:08 +0530

    make menu driven

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -45,15 +45,48 @@ int main(){
     root->right = new Node(30);
     root->left->left = new Node(40);
 
-    inorder(root);
+    int choice;
 
-    cout << "\n";
+    do{
+        cout << "\t\t MENU :" << endl;
+        cout << "1 : Inorder Transversal" << endl;
+        cout << "2 : Preorder Transversal" << endl;
+        cout << "3 : Postorder Transversal" << endl;
+        cout << "4 : Exit the program" << endl;
+        cout << "Please enter your choice : ";
+        cin >> choice;
 
-    preorder(root);
+        switch (choice)
+        {
+        case 1:
+            cout << "Inorder Transversal" << endl;
+            inorder(root);
+            cout << endl;
+            break;
 
-    cout << "\n";
+        case 2:
+            cout << "Preorder Transversal" << endl;
+            preorder(root);
+            cout << endl;
+            break;
 
-    postorder(root);
+        case 3:
+            cout << "Postorder Transversal" << endl;
+            postorder(root);
+            cout << endl;
+            break;
+
+        case 4:
+            cout << "Exiting the program" << endl;
+            exit(1);
+            break;
+        
+        default:
+            cout << "Invalid input. Please try again" << endl;
+            break;
+        }
+
+    }while(choice != 4);
 
     return 0;
 }
