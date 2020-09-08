ae23a002efec65b020ab56e0451ed5d3405be8ee
sidchiku9 <sidchiku9@gmail.com>
2020-09-08 09:12:53 +0530

    remove errors

--- queueLinkedList.cpp
+++ queueLinkedList.cpp
@@ -1,31 +1,37 @@
-#include<stdlib.h>
-#include<iostream>
+#include <stdlib.h>
+#include <iostream>
 
 using namespace std;
 
-struct Node{
+struct Node
+{
     int key;
     Node *next;
 
-    Node(int x){
+    Node(int x)
+    {
         key = x;
         next = NULL;
     }
 };
 
-struct Queue{
+struct Queue
+{
     Node *front, *rear;
     int size;
 
-    Queue(){
+    Queue()
+    {
         front = NULL;
         rear = NULL;
         size = 0;
     }
 
-    void enqueue(int x){
+    void enqueue(int x)
+    {
         Node *temp = new Node(x);
-        if(front == NULL){
+        if (front == NULL)
+        {
             front = rear = temp;
             return;
         }
@@ -34,36 +40,78 @@ struct Queue{
         size++;
     }
 
-    void dequeue(){
-        if(front == NULL){
+    void dequeue()
+    {
+        if (front == NULL)
+        {
             return;
         }
         Node *temp = front;
         front = front->next;
-        if(front == NULL){
+        if (front == NULL)
+        {
             rear = NULL;
         }
-        delete(temp);
+        delete (temp);
         size--;
     }
 
-    void printQueue(){
+    void printQueue()
+    {
         Node *temp = front;
 
-        while(temp != NULL){
+        while (temp != NULL)
+        {
             cout << temp->key << "->";
-            temp = temp -> next;
+            temp = temp->next;
         }
     }
 };
 
-int main(){
+int main()
+{
     struct Queue q;
     int choice, data;
 
-    do{
+    do
+    {
         cout << "\t\t MENU" << endl;
-    }
+        cout << "1 ENQUEUE" << endl;
+        cout << "2 DEQUEUE" << endl;
+        cout << "3 PRINT QUEUE" << endl;
+        cout << "4 EXIT THE PROGRAM" << endl;
+        cout << "Please enter your choice" << endl;
+
+        cin >> choice;
+
+        switch (choice)
+        {
+        case 1:
+            cout << "Enter the data element you want to insert" << endl;
+            cin >> data;
+            q.enqueue(data);
+            break;
+
+        case 2:
+            cout << "Deleting element from the queue" << endl;
+            q.dequeue();
+            break;
+
+        case 3:
+            cout << "Printing the queue : " << endl;
+            q.printQueue();
+            cout << endl;
+            break;
+
+        case 4:
+            exit(1);
+            break;
+
+        default:
+            cout << "Invalid input. Please try again" << endl;
+            break;
+        }
+    } while (choice != 4);
 
     return 0;
 }
