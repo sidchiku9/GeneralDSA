6c618c29ff876d1f7b6722c3bf61d11216735dac
sidchiku9 <sidchiku9@gmail.com>
2020-09-15 16:28:47 +0530

    printGraph()

--- adjacencyList.cpp
+++ adjacencyList.cpp
@@ -9,6 +9,15 @@ void addEdge(vector<int> adj[], int u, int v){
     adj[v].push_back(u);
 }
 
+void printGraph(vector<int> adj[], int v){
+    for(int i = 0 ; i < v ; i++){
+        cout << i <<": ";
+        for(int x : adj[i])
+            cout << x << " ";
+        cout << endl;
+    }
+}
+
 int main(){
     int v = 4;
     vector<int> adjList[v];
@@ -16,5 +25,6 @@ int main(){
     addEdge(adjList, 0, 2);
     addEdge(adjList, 1, 2);
     addEdge(adjList, 1, 3);
+    printGraph(adjList, v);
     return 0;
 }
