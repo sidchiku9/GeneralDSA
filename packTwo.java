4e8804676a7e849c694b6329b79b741ebd611f1e
sidchiku9 <sidchiku9@gmail.com>
2020-09-15 16:19:45 +0530

    some unfinished

---
+++ packTwo.java
@@ -0,0 +1,11 @@
+package p2;
+
+public class packTwo{
+    public int addTwo(int x, int y){
+        return x + y;
+    }
+    public static void main(String[] args){
+        packTwo obj = new packTwo();
+        System.out.println(obj.addTwo(10,20));
+    }
+}
