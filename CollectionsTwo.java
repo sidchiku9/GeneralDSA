edaa5907d4f79100db91d5b7b2617c0662aa4b10
sidchiku9 <sidchiku9@gmail.com>
2020-09-19 22:17:27 +0530

    CollectionsTwo + CollectionsOne modified a little

--- CollectionsTwo.java
+++ CollectionsTwo.java
@@ -1,5 +1,30 @@
+import java.util.*;
+import java.lang.*;
+
 public class CollectionsTwo {
     public static void main(String[] args){
-        
+        ArrayList<Integer> Seq = new ArrayList<Integer>();
+        Seq.add(1);
+        Seq.add(3);
+        Seq.add(4);
+        Seq.add(5);
+        Seq.add(2);
+
+        System.out.println("Unsorted");
+        for(Integer seq : Seq){
+            System.out.println(seq);
+        }
+
+        Collections.sort(Seq);
+        System.out.println("Sorted");
+        for(Integer seq : Seq){
+            System.out.println(seq);
+        }
+
+        Collections.sort(Seq,Collections.reverseOrder());
+        System.out.println("Reverse Order");
+        for(Integer seq : Seq){
+            System.out.println(seq);
+        }
     }
 }
