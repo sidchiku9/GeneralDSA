33b4653e860bb31f2a0468f1ea96b25e4d0c4b19
sidchiku9 <sidchiku9@gmail.com>
2020-10-05 23:07:38 +0530

    .

--- oslabda4_3.cpp
+++ oslabda4_3.cpp
@@ -6,135 +6,139 @@ using namespace std;
 int main()
 {
     int c, i, j, k, n, l, m[10], p[10], po[20], flag, z, y, temp, temp1;
-    cout << "enter memory partition:\t";
+    cout << "Enter the number of memory partitions : \t";
     cin >> n;
-    cout << "\nenter memory size for\n";
+    cout << "\nEnter the memory size for : \n";
     for (i = 1; i <= n; i++)
     {
-        cout << "\npartition " << i << " :\t";
+        cout << "\nPartition " << i << " :\t";
         cin >> m[i];
         po[i] = i;
     }
-    cout << "\nenter process:\t";
+    cout << "\nEnter the number of processes : \t";
     cin >> j;
-    cout << "\nenter memory size for\n";
+    cout << "\nEnter the memory size for : \n";
     for (i = 1; i <= j; i++)
     {
-        cout << "\nprocess " << i << " :\t";
+        cout << "\nProcess " << i << " :\t";
         cin >> p[i];
     }
 
-   do{
-    cout << "\n\t\tMENU\n\t1 : First Fit\n\t2 : Best Fit\n\t3 : Worst Fit\nEnter your choice\t";
-    cin >> c;
-    switch (c)
+    do
     {
-    case 1:
-        for (i = 1; i <= j; i++)
+        cout << "\n\t\tMENU\n\t1 : First Fit\n\t2 : Best Fit\n\t3 : Worst Fit\n\t4 : Exit the program\nEnter your choice\t";
+        cin >> c;
+        switch (c)
         {
-            flag = 1;
-            for (k = 1; k <= n; k++)
+        case 1:
+            cout << "First Fit" << endl;
+            for (i = 1; i <= j; i++)
             {
-                if (p[i] <= m[k])
+                flag = 1;
+                for (k = 1; k <= n; k++)
                 {
-                    cout << "\nprocess " << i << " whose memory size is " << p[i] << "KB allocated at memory partition:\t" << po[k];
-                    m[k] = m[k] - p[i];
-                    break;
+                    if (p[i] <= m[k])
+                    {
+                        cout << "\nProcess " << i << " whose memory size is " << p[i] << "KB allocated at memory partition :\t" << po[k];
+                        m[k] = m[k] - p[i];
+                        break;
+                    }
+                    else
+                    {
+                        flag++;
+                    }
                 }
-                else
+                if (flag > n)
                 {
-                    flag++;
+                    cout << "\nProcess " << i << " whose memory size is " << p[i] << "KB can't be allocated.";
                 }
             }
-            if (flag > n)
+            break;
+        case 2:
+            cout << "Best Fit" << endl;
+            for (y = 1; y <= n; y++)
             {
-                cout << "\nprocess " << i << " whose memory size is " << p[i] << "KB can't be allocated";
+                for (z = y; z <= n; z++)
+                {
+                    if (m[y] > m[z])
+                    {
+                        temp = m[y];
+                        m[y] = m[z];
+                        m[z] = temp;
+                        temp1 = po[y];
+                        po[y] = po[z];
+                        po[z] = temp1;
+                    }
+                }
             }
+            for (i = 1; i <= j; i++)
+            {
+                flag = 1;
+                for (k = 1; k <= n; k++)
+                {
+                    if (p[i] <= m[k])
+                    {
+                        cout << "\nProcess " << i << " whose memory size is " << p[i] << "KB allocated at memory partition:\t" << po[k];
+                        m[k] = m[k] - p[i];
+                        break;
+                    }
+                    else
+                    {
+                        flag++;
+                    }
+                }
+                if (flag > n)
+                {
+                    cout << "\nProcess " << i << " whose memory size is " << p[i] << "KB can't be allocated";
+                }
+            }
+            break;
+        case 3:
+            cout << "Worst Fit" << endl;
+            for (y = 1; y <= n; y++)
+            {
+                for (z = y; z <= n; z++)
+                {
+                    if (m[y] < m[z])
+                    {
+                        temp = m[y];
+                        m[y] = m[z];
+                        m[z] = temp;
+                        temp1 = po[y];
+                        po[y] = po[z];
+                        po[z] = temp1;
+                    }
+                }
+            }
+            for (i = 1; i <= j; i++)
+            {
+                flag = 1;
+                for (k = 1; k <= n; k++)
+                {
+                    if (p[i] <= m[k])
+                    {
+                        cout << "\nProcess " << i << " whose memory size is " << p[i] << "KB allocated at memory partition:\t" << po[k];
+                        m[k] = m[k] - p[i];
+                        break;
+                    }
+                    else
+                    {
+                        flag++;
+                    }
+                }
+                if (flag > n)
+                {
+                    cout << "\nProcess " << i << " whose memory size is " << p[i] << "KB can't be allocated";
+                }
+            }
+            break;
+        case 4:
+            break;
+        default:
+            cout << "Invalid input. Please try again" << endl;
+            break;
         }
-        break;
-    case 2:
-        for (y = 1; y <= n; y++)
-        {
-            for (z = y; z <= n; z++)
-            {
-                if (m[y] > m[z])
-                {
-                    temp = m[y];
-                    m[y] = m[z];
-                    m[z] = temp;
-                    temp1 = po[y];
-                    po[y] = po[z];
-                    po[z] = temp1;
-                }
-            }
-        }
-        for (i = 1; i <= j; i++)
-        {
-            flag = 1;
-            for (k = 1; k <= n; k++)
-            {
-                if (p[i] <= m[k])
-                {
-                    cout << "\nprocess " << i << " whose memory size is " << p[i] << "KB allocated at memory partition:\t" << po[k];
-                    m[k] = m[k] - p[i];
-                    break;
-                }
-                else
-                {
-                    flag++;
-                }
-            }
-            if (flag > n)
-            {
-                cout << "\nprocess " << i << " whose memory size is " << p[i] << "KB can't be allocated";
-            }
-        }
-        break;
-    case 3:
-        for (y = 1; y <= n; y++)
-        {
-            for (z = y; z <= n; z++)
-            {
-                if (m[y] < m[z])
-                {
-                    temp = m[y];
-                    m[y] = m[z];
-                    m[z] = temp;
-                    temp1 = po[y];
-                    po[y] = po[z];
-                    po[z] = temp1;
-                }
-            }
-        }
-        for (i = 1; i <= j; i++)
-        {
-            flag = 1;
-            for (k = 1; k <= n; k++)
-            {
-                if (p[i] <= m[k])
-                {
-                    cout << "\nprocess " << i << " whose memory size is " << p[i] << "KB allocated at memory partition:\t" << po[k];
-                    m[k] = m[k] - p[i];
-                    break;
-                }
-                else
-                {
-                    flag++;
-                }
-            }
-            if (flag > n)
-            {
-                cout << "\nprocess " << i << " whose memory size is " << p[i] << "KB can't be allocated";
-            }
-        }
-        break;
-    case 4 :
-        break;
-    default :
-        cout << "Invalid input. Please try again" << endl;
-        break;
-    }
-}while(c != 4);
+    } while (c != 4);
 
     return 0;
 }
