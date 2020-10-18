299851f1921e94f945e6d551eca48f93811746a8
sidchiku9 <sidchiku9@gmail.com>
2020-10-18 16:43:16 +0530

    .

--- TkinterOne.py
+++ TkinterOne.py
@@ -1,8 +1,10 @@
 import tkinter
 from tkinter import *
+from sqlalchemy import create_engine
 import mysql.connector
 
 db_connection = mysql.connector.connect(host = '127.0.0.1', user = 'root', password = 'Chiku$!d9',auth_plugin='mysql_native_password', database = 'Goconstruct')
+myCursor = db_connection.cursor()
 
 main = tkinter.Tk()
 main.title('Entry Window')
@@ -20,8 +22,46 @@ def CustomerDial() :
     GivesProjects.pack(padx=20, pady=20)
 
 def NeedItem() :
+    global t1, t2, t3, t4
+
+    my_w = tkinter.Tk()
+    my_w.geometry("400x400")
+    my_w.title("Needs Items")
+    l0 = tkinter.Label(my_w,  text='Needs Items',font=('Helvetica', 16), width=30,anchor="c" )
+    l0.grid(row=1,column=1,columnspan=4)
+
+    l1 = tkinter.Label(my_w,  text='Item Number : ', width=10,anchor="c" )  
+    l1.grid(row=3,column=1)
+    t1 = tkinter.Text(my_w,  height=1, width=10,bg='white') 
+    t1.grid(row=3,column=2) 
     
-    print('Needs items') #insert sql query
+    l2 = tkinter.Label(my_w,  text='Quantity : ', width=10,anchor="c" )  
+    l2.grid(row=4,column=1)
+    t2 = tkinter.Text(my_w,  height=1, width=10,bg='white') 
+    t2.grid(row=4,column=2) 
+
+    l3 = tkinter.Label(my_w,  text='Price : ', width=10,anchor="c" )  
+    l3.grid(row=5,column=1)
+    t3 = tkinter.Text(my_w,  height=1, width=10,bg='white') 
+    t3.grid(row=5,column=2) 
+
+    l4 = tkinter.Label(my_w,  text='Order : ', width=10,anchor="c" )  
+    l4.grid(row=6,column=1)
+    t4 = tkinter.Text(my_w,  height=1, width=10,bg='white') 
+    t4.grid(row=6,column=2) 
+
+    b1 = tkinter.Button(my_w,  text='Add Record', width=10, command=lambda: add_data())  
+    b1.grid(row=7,column=2) 
+
+    my_str = tkinter.StringVar()
+    my_str.set("Output")
+
+def add_data() :
+    my_name = t1.get("1.0",END) # read name
+    my_class = t2.get("1.0",END)    # read class
+    my_mark = t3.get("1.0",END) # read mark
+    my_gender = t4.get("1.0",END)
+    print("Query inserted")
 
 def MakePayment() :
     print('Make payment')
