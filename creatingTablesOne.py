df308efb0946565f118f225eb3677d8edde84b63
sidchiku9 <sidchiku9@gmail.com>
2020-10-18 13:51:51 +0530

    .

--- creatingTablesOne.py
+++ creatingTablesOne.py
@@ -21,18 +21,16 @@ for i in execute :
 mydb.commit()
 
 
-
-#vendor : Name VARCHAR(50), Phone BIGINT , Email VARCHAR(50), ShopName VARCHAR(50), 
-# Address VARCHAR(50), Items VARCHAR(50), PRIMARY KEY(Name, Phone)
-
-#project : ProjectNumber INT, CustomerName VARCHAR(100), CustomerNumber BIGINT, 
-#VendorNumber BIGINT, Item INT, Details VARCHAR(50), PRIMARY KEY(ProjectNumber, CustomerName, CustomerNumber)
-#payment : Name VARCHAR(50), Phone BIGINT, Address VARCHAR(50), Item VARCHAR(50), PRIMARY KEY(Name, Phone)
-
 '''
 DONE : 
 #customer : Name VARCHAR(100), Age INT, Address VARCHAR(20) PRIMARY KEY, Project VARCHAR(20), Date DATE
 #admin : AdminID INT PRIMARY KEY, Password VARCHAR(30), Logs VARCHAR(30)
 #item : ItemNumber INT PRIMARY KEY, Quantity INT, Price INT, Order VARCHAR(50)
-#order : CustomerName VARCHAR(50), Phone BIGINT PRIMARY KEY, Address VARCHAR(50), Amount INT, Project VARCHAR(50), Item INT
+#order : CustomerName VARCHAR(50), Phone BIGINT PRIMARY KEY, Address VARCHAR(50), Amount INT, Project VARCHAR(50), Item INT)
+#vendor : Name VARCHAR(50), Phone BIGINT , Email VARCHAR(50), ShopName VARCHAR(50), 
+# Address VARCHAR(50), Items VARCHAR(50), PRIMARY KEY(Name, Phone)
+#project : ProjectNumber INT, CustomerName VARCHAR(100), CustomerNumber BIGINT, 
+#VendorNumber BIGINT, Item INT, Details VARCHAR(50), PRIMARY KEY(ProjectNumber, CustomerName, CustomerNumber)
+#payment : Name VARCHAR(50), Phone BIGINT, Address VARCHAR(50), Item VARCHAR(50), PRIMARY KEY(Name, Phone)
+
 '''
